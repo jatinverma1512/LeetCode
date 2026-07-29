f5a1dc090d25576036df2a1f05801c011b81971e
Jatin Verma <jvermangh@gmail.com>
2026-07-29 22:19:14 +0530

    Time: 174 ms (6.1%), Space: 39.8 MB (5.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,23 +4,23 @@ public:
         int low = 0;
         int ans = INT_MIN;
 
-        unordered_map<char,int> freq;
-        
-        if(s.length() == 0) return 0;
+        if(s.length() == 0) return 0; 
 
-        for(int high = 0; high<s.length(); high++){
-            freq[s[high]]++;
+        unordered_map<char,int>mp;
 
-            while(freq[s[high]] > 1){
-                freq[s[low]]--;
-                if(freq[s[low]] == 0){
-                    freq.erase(s[low]);
+        for(int high = 0; high < s.length(); high++){
+            mp[s[high]]++;
+
+            while(mp[s[high]] > 1){
+                mp[s[low]]--;
+                if(mp[s[low]] == 0){
+                    mp.erase(s[low]);
                 }
                 low++;
             }
 
-            int len = high-low+1;
-            ans = max(ans,len);
+            int len = high - low +1;
+            ans = max(ans, len);
         }
         return ans;
     }
