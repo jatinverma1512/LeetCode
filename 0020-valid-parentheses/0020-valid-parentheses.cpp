1a62e3a2d5607e759b530d5b86344c83152d0ec6
Jatin Verma <jvermangh@gmail.com>
2026-08-01 16:53:42 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.43%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char ch: s){
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }
+            else if(ch == ')' && !st.empty() && st.top() == '('){
+                st.pop();
+            }
+            else if(ch == ']' && !st.empty() && st.top() == '['){
+                st.pop();
+            }
+            else if(ch == '}' && !st.empty() && st.top() == '{'){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+        }
+
+        if(st.size() == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
